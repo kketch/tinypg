18a338a6fb906ea4a7689d907911fd3395429256
Kevin Wenner <wennerkevin@gmail.com>
2025-09-25 10:27:23 +0000

    format

--- src/tinypg/__init__.py
+++ src/tinypg/__init__.py
@@ -4,28 +4,28 @@ TinyPG: Ephemeral PostgreSQL databases for Python development and testing.
 Based on ephemeralpg by Eric Radman, reimplemented in pure Python.
 """
 
-from .core import EphemeralDB, AsyncEphemeralDB
-from .context import database, async_database, database_pool
 from .config import TinyPGConfig
+from .context import async_database, database, database_pool
+from .core import AsyncEphemeralDB, EphemeralDB
 from .exceptions import (
-    TinyPGError,
-    DatabaseStartError,
     BinaryNotFoundError,
-    DownloadError,
+    DatabaseStartError,
     DatabaseTimeoutError,
+    DownloadError,
+    TinyPGError,
 )
 
 __version__ = "0.1.0"
 
 __all__ = [
     "EphemeralDB",
-    "AsyncEphemeralDB", 
+    "AsyncEphemeralDB",
     "database",
     "async_database",
     "database_pool",
     "TinyPGConfig",
     "TinyPGError",
-    "DatabaseStartError", 
+    "DatabaseStartError",
     "BinaryNotFoundError",
     "DownloadError",
     "DatabaseTimeoutError",
